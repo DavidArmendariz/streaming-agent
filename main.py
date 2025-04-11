ebc4c44170ed071495ae76dd084bdc2ad353653d
David Armendariz <david.adrian.a.p@gmail.com>
2025-04-11 15:12:15 -0400

    Adds back default settings for VAD

--- main.py
+++ main.py
@@ -62,11 +62,6 @@ async def send_session_update(openai_ws):
         "session": {
             "turn_detection": {
                 "type": "server_vad",
-                "threshold": 0.5,
-                "prefix_padding_ms": 300,
-                "silence_duration_ms": 500,
-                "create_response": True,
-                "interrupt_response": True,
             },
             "input_audio_format": "g711_ulaw",
             "output_audio_format": "g711_ulaw",
